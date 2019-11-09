3c5a18c88f8e821fc876f83d4f0321f791362745
Abel-Moremi <abelmoremiz@gmail.com>
2019-11-09 15:09:01 +0200

    Initial skeletal definition of methods

---
+++ GraphTown.java
@@ -0,0 +1,31 @@
+public class GraphTown{
+
+    // Constructor to that takes in data 
+    public GraphTown(){}
+
+    // inserts an edge between any two towns/cities 
+    // passed as argument to this method
+    public insertEdge(){}
+
+    // deletes an edge between any two towns passed as 
+    // argument to this method
+    public deleteEdge(){}
+
+    // inserts a town into your graph data structure. 
+    // This call for assigning edges to at least one other 
+    // town in order to make sure that the graph is connected
+    public insertTown(){}
+
+    // delete any town that is supplied to this method as an 
+    // argument. Note that when you delete a town, you must 
+    // delete all edges to all other towns before deleting this town
+    public deleteTown(){}
+
+    // pass a list of towns as arguments to this method which declares
+    // the list as neighbors of this town.
+    public assignNeighbours(){}
+
+    //create a minimum spanning tree and print such tree for us to see
+    public showMST(){}
+
+}
